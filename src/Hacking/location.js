a56611077c33eec9b2b8f1bba29e132810848395
XDitamar <itamardanon@gmail.com>
2023-07-31 16:25:43 +0300

    8/commit

--- src/Hacking/location.js
+++ src/Hacking/location.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./LocationComponent.css"; // Import the CSS file for custom styling
 
 const LocationComponent = () => {
   const [locationData, setLocationData] = useState(null);
@@ -42,8 +45,39 @@ const LocationComponent = () => {
       const response = await fetch("https://api.ipify.org?format=json");
       const data = await response.json();
       setIpData(data);
+
+      // Get additional information using the IP address from ipify
+      if (data.ip) {
+        fetchLocationData(data.ip);
+      }
     } catch (error) {
       setIpData("Failed to fetch IP address.");
+    }
+  };
+
+  const fetchLocationData = async (ipAddress) => {
+    try {
+      const response = await fetch(`https://ipapi.co/${ipAddress}/json/`);
+      const data = await response.json();
+      setLocationData((prevData) => ({
+        ...prevData,
+        isp: data.org,
+        country: data.country_name,
+        city: data.city,
+        device: getDevice(),
+      }));
+    } catch (error) {
+      console.error("Failed to fetch location data.", error);
+    }
+  };
+
+  const getDevice = () => {
+    const { userAgent } = window.navigator;
+    if (/OnePlus[\s\S]*ONEPLUS[A-Z\d]{5}/i.test(userAgent)) {
+      return "OnePlus 8"; // Customized detection for OnePlus 8
+    } else {
+      // You can add more custom device detection here based on the userAgent
+      return "Unknown";
     }
   };
 
@@ -61,10 +95,10 @@ const LocationComponent = () => {
     return browserMatch ? browserMatch[0] : "Unknown";
   };
 
-  const { latitude, longitude, timestamp } = locationData || {};
+  const { latitude, longitude, timestamp, isp, country, city, device } = locationData || {};
 
   return (
-    <div>
+    <div className="location-container">
       {permissionDenied ? (
         <p>Location access is denied. Please enable location services to use this feature.</p>
       ) : (
@@ -81,12 +115,35 @@ const LocationComponent = () => {
             <h3>User System Information:</h3>
             <p>Operating System: {getOperatingSystem()}</p>
             <p>Browser: {getBrowserInfo()}</p>
+            {device && <p>Device: {device}</p>}
           </div>
 
           <div>
             <h3>IP Data:</h3>
             {ipData ? <p>IP Address: {ipData.ip}</p> : <p>Loading IP address...</p>}
           </div>
+
+          {latitude && longitude && (
+            <div>
+              <h3>Map:</h3>
+              <MapContainer center={[latitude, longitude]} zoom={13} className="map-container">
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={[latitude, longitude]}>
+                  <Popup>
+                    {country && <p>Country: {country}</p>}
+                    {city && <p>City: {city}</p>}
+                    {device && <p>Device: {device}</p>}
+                    <p>Latitude: {latitude}</p>
+                    <p>Longitude: {longitude}</p>
+                    {timestamp && <p>Timestamp: {timestamp}</p>}
+                  </Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+          )}
         </div>
       )}
     </div>
